8158fb534fda1a602cd6f372351f5ed3adc6107a
amir07170418 <hossein13810418@gmail.com>
2026-08-02 23:39:06 +0330

    employee controller complete

---
+++ src/main/java/org/example/university/controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package org.example.university.controller;
+
+import org.example.university.dto.EmployeeRequest;
+import org.example.university.dto.EmployeeResponse;
+import org.example.university.service.EmployeeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{id}")
+    public EmployeeResponse findById(@PathVariable Long id) {
+        return employeeService.findById(id);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public Page<EmployeeResponse> findAll(Pageable pageable) {
+        return employeeService.findAll(pageable);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public  EmployeeResponse save(@RequestBody EmployeeRequest employeeRequest) {
+        return employeeService.save(employeeRequest);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}")
+    public EmployeeResponse update(@PathVariable Long id, @RequestBody EmployeeRequest employeeRequest) {
+        return employeeService.update(id, employeeRequest);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        employeeService.delete(id);
+    }
+
+}
